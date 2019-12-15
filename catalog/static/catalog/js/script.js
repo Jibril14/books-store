ecf4b0e7b5b11efd775e026ea26f08df56f05ddc
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2019-12-15 10:37:07 +0100

    Add More Static

--- catalog/static/catalog/js/script.js
+++ catalog/static/catalog/js/script.js
@@ -2,7 +2,7 @@
 const open_menu = document.querySelector('.menu')
 const nav_container = document.querySelector('.nav-container')
   
-const mobile_logo =document.querySelector('.logo') 
+const mobile_logo = document.querySelector('.logo') 
 
 open_menu.addEventListener('click', () =>{
  nav_container.classList.toggle('active')
@@ -11,10 +11,11 @@ open_menu.addEventListener('click', () =>{
 })
 
 
+
 const counters = document.querySelectorAll(".counter")
 console.log("c",counters)
 counters.forEach(count=>{
-    count.innerText = '0'
+    count.innerText = '0' 
     const updateCounter = () => {
       const target = Number(count.getAttribute('data-target'))
       const c = Number(count.innerText)
@@ -33,3 +34,24 @@ counters.forEach(count=>{
     updateCounter()
 })
 
+
+const profile = document.querySelector('.profile-card')
+const card_content = document.querySelector('.card-content')
+
+window.addEventListener("scroll", showProfile)
+
+function showProfile(){
+  const triggerPoint = window.innerHeight * 0.8
+  console.log("TriggerPoint",triggerPoint)
+
+        const profileTop = profile.getBoundingClientRect().top
+        console.log("ProfileTop",profileTop)
+        if(profileTop < triggerPoint){
+            profile.classList.add('show')
+            card_content.classList.add('show')
+        } else{
+            profile.classList.remove('show')
+            card_content.classList.remove('show')
+        }
+  
+}
