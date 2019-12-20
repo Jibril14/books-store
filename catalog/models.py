97354a03b7e68e5e8ced0fb7d26cf85f60c45437
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2019-12-20 23:01:39 +0100

    Add More Fields

--- catalog/models.py
+++ catalog/models.py
@@ -1,21 +1,30 @@
 from django.db import models
 from django.urls import reverse
-
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import date
+from django.contrib.auth.models import User
 
 class Genre(models.Model):
-    name = models.CharField(max_length=200, help_text='Enter the book genre(e.g Science, Fiction)')
+    name = models.CharField(max_length=200, help_text='Enter the book genre(e.g Drama, Fiction)')
 
     def __str__(self):
         return self.name
 
-
 class Book(models.Model):
     title = models.CharField(max_length = 100)
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True,  help_text='We only accept books from these authors')
     summary = models.TextField(max_length=600, help_text='Enter a brief description')
     isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    rating = models.IntegerField(
+        default=1, 
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    book_cover = models.CharField(
+        max_length=500,
+        help_text='Enter image link',
+        blank=True,
+        default="https://res.cloudinary.com/webmonc/image/upload/v1658887052/portfolio/book-store/default_img_hfhgpg.jpg")
 
     def __str__(self):
         return self.title
@@ -23,14 +32,23 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
 
+    # For admin
+    def display_genre(self):
+        return ',  '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'First 3 Genre'
+
+   
+
 import uuid
 class BookCopy(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique id of this book')
-    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
-    imprint = models.CharField(max_length=200)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text='Unique id of this book')
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True, help_text='Pls select a book to store')
+    imprint = models.CharField(max_length=200,  help_text='e.g (First Edition Vista Printing Press)' )
     due_back = models.DateField(null=True, blank=True)
-    #Rating
-    #Picture
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True ,)
     
     Book_Status = (
         ('m', 'Maintenance'),
@@ -44,26 +62,38 @@ class BookCopy(models.Model):
         max_length=1,
         blank=True,
         default='a',
-        help_text='Book status'
-         
+        help_text='Book status' 
         )
 
     class Meta:
         ordering = ['due_back']
 
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
     def __str__(self):
-        return f'{self.id} ({self.book.title})'
+        return f'{self.id} ({self.book.title}) by {self.book.author}'
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.book.id)])
+
         
 class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
 
     class Meta:
-        ordering = ['last_name', 'first_name']
+        ordering = ['lastname', 'firstname']
 
     def get_absolute_url(self):
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
-        return f'{self.last_name}, {self.first_name}'    
+        return f'{self.lastname}, {self.firstname}'    
